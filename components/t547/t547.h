f4530aaf1f585431e7da68965db2c80936ff9109
SrLPZ <76914115+SrLPZ@users.noreply.github.com>
2023-03-18 10:51:38 +0100

    Update t547.h

--- components/t547/t547.h
+++ components/t547/t547.h
@@ -25,6 +25,9 @@ class T547 : public PollingComponent, public display::DisplayBuffer {
   void display();
   void clean();
   void update() override;
+ 
+  void eink_off_();
+  void eink_on_();
 
   void setup() override;
 
@@ -39,9 +42,6 @@ class T547 : public PollingComponent, public display::DisplayBuffer {
 
  protected:
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
-
-  void eink_off_();
-  void eink_on_();
 
 
   int get_width_internal() override { return 960; }
